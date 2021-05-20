55c8cf96e1e05bc1565da4b7fb7ab12a665c9191
Santi <santi.sanchez88@gmail.com>
2021-05-20 16:54:41 +0100

    neighbors function

--- main.py
+++ main.py
@@ -73,7 +73,19 @@ class Spot:
             win, self.color, (self.x, self.y, self.width, self.width))
 
     def update_neighbors(self, grid):
-        pass
+        self.neighbors = []
+        # DOWN
+        if self.row < self.total_rows - 1 and not grid[self.row + 1][self.col].is_barrier():
+            self.neighbors.append(grid[self.row + 1][self.col])
+        # UP
+        if self.row > 0 and not grid[self.row - 1][self.col].is_barrier():
+            self.neighbors.append(grid[self.row - 1][self.col])
+        # LEFT
+        if self.row > 0 and not grid[self.row][self.col - 1].is_barrier():
+            self.neighbors.append(grid[self.row][self.col - 1])
+        # RIGHT
+        if self.row < self.total_rows - 1 and not grid[self.row][self.col + 1].is_barrier():
+            self.neighbors.append(grid[self.row][self.col + 1])
 
     def __lt__(self, other):
         return False
@@ -167,6 +179,10 @@ def main(win, width):
                     start = None
                 elif spot == end:
                     end = None
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and not started:
+                    pass
 
     pygame.quit()
 
